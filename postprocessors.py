2805c37365c9308c213e40a62c68e6b84e2dca07
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-07-02 01:12:35 -0700

    generic helper for day_fill and week_fill postprocessors

--- postprocessors.py
+++ postprocessors.py
@@ -1,22 +1,32 @@
 # coding: utf-8
 
-import datetime
+from datetime import timedelta
 from helpers import make_record
 
 
-def day_fill(data, fill_value = None):
+def day_fill(data, fill_value=None):
     """Given a data set with missing day values sorted by day, adds records
     with value of `fill_value`
     """
-    
+    return generic_date_fill(1, data, fill_value)
+
+
+def week_fill(data, fill_value=None):
+    """Given a sorted data set with missing week keys, adds records with
+    value of `fill_value`
+    """
+    return generic_date_fill(7, data, fill_value)
+
+
+def generic_date_fill(day_interval, data, fill_value=None):
     new_data = list()
     prev = None
     for dt in data:
         if prev:
-            day_diff = (dt['key'] - prev['key']).days
-            if day_diff > 1:
-                for i in range(1, day_diff):
-                    new_date = prev['key'] + datetime.timedelta(days = i)
+            diff = (dt['key'] - prev['key']).days / day_interval
+            if diff > 1:
+                for i in range(1, diff):
+                    new_date = prev['key'] + timedelta(days=i*day_interval)
                     new_data.append(make_record(new_date, fill_value))
         new_data.append(dt)
         prev = dt
